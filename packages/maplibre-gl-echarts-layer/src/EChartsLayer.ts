5a8e4135256192c1dd586809aec326ccb5627cfd
huanglii <naivemap@163.com>
2025-07-26 13:24:18 +0800

    update docs

--- packages/maplibre-gl-echarts-layer/src/EChartsLayer.ts
+++ packages/maplibre-gl-echarts-layer/src/EChartsLayer.ts
@@ -13,7 +13,8 @@ import {
 import maplibregl from 'maplibre-gl'
 
 /**
- * EChartsLayer 配置项
+ * A composite type for ECharts options used by the EChartsLayer.
+ * It includes common components and series types like Lines and Scatter.
  */
 export type ECOption = ComposeOption<
   | TitleComponentOption
@@ -76,13 +77,40 @@ class CoordinateSystem {
 }
 
 /**
- * Integrate the **Lines graph** and **Scatter (bubble)** chart of Apache ECharts.
+ * A custom MapLibre GL JS layer that renders Apache ECharts visualizations.
+ *
+ * @remarks
+ * This layer integrates ECharts by creating a dedicated canvas over the map,
+ * allowing for rich data visualizations using ECharts' powerful charting capabilities.
+ * It is optimized for `lines` and `scatter` series types to visualize flows,
+ * trajectories, and point distributions.
+ *
+ * The layer automatically synchronizes the ECharts view with the map's panning and zooming.
+ *
  * @example
  * ```ts
- * import EChartsLayer from '@naivemap/maplibre-gl-echarts-layer'
+ * import EChartsLayer from '@naivemap/maplibre-gl-echarts-layer';
  *
- * const layer = new EChartsLayer('echarts-layer', option)
- * map.addLayer(layer)
+ * // 1. Define a standard ECharts option object.
+ * const option = {
+ *   series: [{
+ *     type: 'scatter',
+ *     name: 'Cities',
+ *     data: [
+ *       // Data format: [longitude, latitude]
+ *       [-74.0060, 40.7128],
+ *       [-0.1278, 51.5074],
+ *       [139.6917, 35.6895]
+ *     ],
+ *     symbolSize: 10,
+ *   }]
+ * };
+ *
+ * // 2. Create the layer instance
+ * const layer = new EChartsLayer('echarts-layer', option);
+ *
+ * // 3. Add the layer to the map
+ * map.addLayer(layer);
  * ```
  */
 export default class EChartsLayer implements maplibregl.CustomLayerInterface {
@@ -103,7 +131,8 @@ export default class EChartsLayer implements maplibregl.CustomLayerInterface {
 
   /**
    * @param id - A unique layer id
-   * @param ecOption - ECharts configuration options
+   * @param ecOption - The ECharts option object used to configure the visualization.
+   * @see https://echarts.apache.org/en/option.html
    */
   constructor(id: string, ecOption: ECOption) {
     this.id = id
@@ -134,11 +163,17 @@ export default class EChartsLayer implements maplibregl.CustomLayerInterface {
   }
 
   /**
-   * Configuration item and data.
-   * @param option - ECharts configuration options
+   * Updates the ECharts visualization with a new configuration.
+   * This is the primary method for dynamically changing the displayed data or styles.
+   *
+   * @param option - The new ECharts option object to apply.
+   * @param notMerge - If true, the new options will completely replace the existing ones.
+   *                   If false or undefined, the new options will be merged with the old ones.
+   *                   Defaults to `false`.
+   * @see https://echarts.apache.org/en/api.html#echartsInstance.setOption
    */
-  setOption(option: ECOption) {
-    this._ec?.setOption(option)
+  setOption(option: ECOption, notMerge?: boolean) {
+    this._ec?.setOption(option, notMerge)
   }
 
   /**
